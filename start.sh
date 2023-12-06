77f1e25500cffe6e0acbeead5d2020d374bf870c
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2023-12-06 22:28:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/TELLYHUBCLOUD/iTSOK_BOT-3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /iTSOK_BOT-3
 fi
-cd /EvaMaria
+cd /iTSOK_BOT-3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
